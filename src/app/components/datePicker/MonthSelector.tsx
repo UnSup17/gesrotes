750398bf2ac4c0e779439f5da6153ab43460b708
unsup <collazosdie@unicauca.edu.co>
2024-12-02 16:08:51 -0500

    refactor: improve week selector logic and DatePicker components; handle undefined week numbers and streamline date handling
    known problems: there's a bug between the month selection and week selector

--- src/app/components/datePicker/MonthSelector.tsx
+++ src/app/components/datePicker/MonthSelector.tsx
@@ -1,25 +1,19 @@
 import { months } from ".";
 
 interface IMonthSelector {
-  monthSelected: number | undefined;
   handleMonthSelect: (monthIndex: number) => void;
 }
 
-export default function MonthSelector({
-  monthSelected,
-  handleMonthSelect,
-}: IMonthSelector) {
+export default function MonthSelector({ handleMonthSelect }: IMonthSelector) {
   return (
-    <div className="grid grid-cols-4 gap-4 mt-4">
+    <div className="flex flex-col gap-4 mt-4">
       {months.map((month, index) => (
         <button
           key={month}
-          className={`p-2 rounded ${
-            index === monthSelected ? "bg-blue-500 text-white" : "bg-gray-200"
-          }`}
+          className={`p-2 rounded bg-gray-200`}
           onClick={() => handleMonthSelect(index)}
         >
-          {month}
+          {month + " - " + index}
         </button>
       ))}
     </div>
